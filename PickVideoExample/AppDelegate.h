95abea2b3426354e6d92fe351b8710b75c18365e
pasha4uu <pasha7864u@gmail.com>
2018-10-12 21:34:28 +0530

    Initial Commit

---
+++ PickVideoExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PickVideoExample
+//
+//  Created by PASHA on 12/10/18.
+//  Copyright © 2018 Pasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
